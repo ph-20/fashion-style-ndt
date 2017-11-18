38111cbeb5cce0402e6122557fa781f9091d89ff
Thanh <thanhcit15i3@gmail.com>
2017-11-17 23:15:36 -0800

    ft-thanhdt-database

--- app/Product.php
+++ app/Product.php
@@ -17,7 +17,7 @@ class Product extends Model
         $this->belongsTo('Shop\Category', 'category_id', 'id');
     }
 
-    public function orderDetail()
+    public function orderDetails()
     {
         $this->hasMany('Shop\OrderDetail', 'product_id', 'id');
     }
