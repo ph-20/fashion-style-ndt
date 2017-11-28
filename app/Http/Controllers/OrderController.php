b20eca990164809aa6bf488fbbf70d8dc0442c17
NamNT <thanhnamdk2710@gmail.com>
2017-11-28 22:38:56 +0700

    ft-namnt-orderProduct: Code Order Products

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -31,28 +31,6 @@ class OrderController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      *
      * @param  int $id
