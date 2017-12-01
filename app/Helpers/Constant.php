50e0ebfd1891715ecead4e2c9e8bddfc4f6758e6
Thanh <thanhcit15i3@gmail.com>
2017-12-01 20:27:25 +0700

    ft-namnt-search:

--- app/Helpers/Constant.php
+++ app/Helpers/Constant.php
@@ -2,6 +2,6 @@
 define('ADMIN', 1);
 define('USER', 0);
 define('PAGE_SIZE_DEFAULT', 10);
-define('TYPE_CATEGORY_NAM',0);
-define('TYPE_CATEGORY_NU',1);
-define('LINKS',4);
+define('TYPE_CATEGORY_NAM', 0);
+define('TYPE_CATEGORY_NU', 1);
+define('LINKS', 4);
